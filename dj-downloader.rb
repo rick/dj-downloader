8d6c197c72012668525e7623049531521cde9fb4
Rick Bradley <rick@rickbradley.com>
2023-05-21 19:09:55 -0500

    Add logging, fix some shit

--- dj-downloader.rb
+++ dj-downloader.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
+require "yaml"
+require "fileutils"
+
+# ensure we log all output and errors to our logfile
 log_dir = File.join(File.dirname(__FILE__), 'log')
-$stdout.reopen(File.join("dj-downloader.log", "w"))
+$stdout.reopen(File.join(log_dir, "dj-downloader.log"), "w")
 $stdout.sync = true
 $stderr.reopen($stdout)
 
@@ -29,7 +33,7 @@ files_to_download = ARGV
 
 # download each file
 files_to_download.each_with_index do |file, index|
-  puts "Downloading #{file} (#{index + 1}/#{files_to_download.length)"
+  puts "Downloading #{file} (#{index + 1}/#{files_to_download.length})"
   `#{config['dj_downloader_path']} --paths #{config['output_dir']} --restrict-filenames --no-mtime --newline -x --audio-format mp3 #{file} `
 end
 
